fadd088062042d2044e37567c51ea4c626a9e6c1
jeong <ywb102@naver.com>
2025-08-12 14:02:25 +0900

    firebase storage 에 파일 업로드 가능 하도록 수정

--- term/src/main/java/self/infra/UploadController.java
+++ term/src/main/java/self/infra/UploadController.java
@@ -1,6 +1,7 @@
 package self.infra;
 
 import com.google.firebase.cloud.StorageClient;
+import org.springframework.beans.factory.annotation.Autowired; // Autowired import
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,6 +12,14 @@ import java.io.IOException;
 @RequestMapping("/api")
 public class UploadController {
 
+    // StorageClient를 Bean으로 주입받음
+    private final StorageClient storageClient;
+
+    @Autowired
+    public UploadController(StorageClient storageClient) {
+        this.storageClient = storageClient;
+    }
+
     @PostMapping("/upload")
     public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file) {
         if (file.isEmpty()) {
@@ -18,12 +27,12 @@ public class UploadController {
         }
 
         try {
-            String bucketName = StorageClient.getInstance().bucket().getName();
+            // 주입받은 storageClient 사용
+            String bucketName = storageClient.bucket().getName();
             String blobString = "uploads/" + file.getOriginalFilename();
 
-            StorageClient.getInstance()
-                    .bucket(bucketName)
-                    .create(blobString, file.getBytes(), file.getContentType());
+            storageClient.bucket(bucketName)
+                         .create(blobString, file.getBytes(), file.getContentType());
 
             return ResponseEntity.ok(Map.of(
                     "message", "파일 업로드 성공: " + file.getOriginalFilename()
